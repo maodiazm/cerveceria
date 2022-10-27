bb1ab29e4e71834617d126a4aa8d42896a1816f1
Mauricio Diaz <maodiazm1@gmail.com>
2022-10-26 23:39:43 -0500

    desarrollo inicial

---
+++ app/src/main/java/com/example/lahermandad/view/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lahermandad.view.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lahermandad.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
