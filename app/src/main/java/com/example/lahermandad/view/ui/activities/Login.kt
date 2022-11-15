7777e2b634df81e7f6655dc83a0d99b076b9c858
Jesus Adrian Peñaranda Perez <gsusp18@gmail.com>
2022-11-15 03:40:27 -0500

    firebase usuario contraseña

--- app/src/main/java/com/example/lahermandad/view/ui/activities/Login.kt
+++ app/src/main/java/com/example/lahermandad/view/ui/activities/Login.kt
@@ -4,34 +4,58 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.example.lahermandad.R
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class login : AppCompatActivity() {
 
     lateinit var registrobutton : Button
     lateinit var iniciobutton : Button
     lateinit var recuperarbutton : TextView
+    private lateinit var firebaseAuth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        firebaseAuth = Firebase.auth
 
         iniciobutton=findViewById(R.id.Binicio)
         registrobutton=findViewById(R.id.BRegistro)
         recuperarbutton=findViewById(R.id.BRecuperar)
+        val correo=findViewById<EditText>(R.id.correolog)
+        val contrasena=findViewById<EditText>(R.id.contrasenalog)
+
+        iniciobutton.setOnClickListener {
+            log(correo.text.toString(),contrasena.text.toString())
+        }
 
         registrobutton.setOnClickListener {
             startActivity(Intent( this,RegistroActivity::class.java ))
         }
 
-        iniciobutton.setOnClickListener {
-            startActivity(Intent( this,HomeActivity::class.java ))
-        }
-
         recuperarbutton.setOnClickListener {
             startActivity(Intent( this,RecuperarActivity::class.java ))
         }
-
     }
+    private fun log(correo:String, contrasena:String){
+        firebaseAuth.signInWithEmailAndPassword(correo,contrasena)
+            .addOnCompleteListener(this) {
+                    task-> if(task.isSuccessful){
+                val user = firebaseAuth.currentUser
+                Toast.makeText(baseContext,user?.uid.toString(), Toast.LENGTH_SHORT).show()
+                startActivity(Intent( this, HomeActivity::class.java ))
+            }else{
+                Toast.makeText(baseContext,"Error en Usuario o Contraseña", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+}
 }
--- app/src/main/java/com/example/lahermandad/view/ui/activities/RegistroActivity.kt
+++ app/src/main/java/com/example/lahermandad/view/ui/activities/RegistroActivity.kt
@@ -4,17 +4,38 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.example.lahermandad.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegistroActivity : AppCompatActivity() {
     lateinit var registrobutton:Button
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+        firebaseAuth = Firebase.auth
         registrobutton = findViewById(R.id.registrobutton)
+        val correo=findViewById<EditText>(R.id.correo)
+        val contrasena=findViewById<EditText>(R.id.password)
+
         registrobutton.setOnClickListener {
-            startActivity(Intent(this,login::class.java))
+            crearUsuario(correo.text.toString(),contrasena.text.toString())
         }
 
+    }
+    private fun crearUsuario(correo:String, contrasena:String){
+        firebaseAuth.createUserWithEmailAndPassword(correo,contrasena)
+            .addOnCompleteListener(this) {
+                Task->if(Task.isSuccessful){
+                    Toast.makeText(baseContext,"Usuario Creado Exitosamente",Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this,login::class.java))
+                }else{
+                    Toast.makeText(baseContext, "Error al crear Usuario",Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
