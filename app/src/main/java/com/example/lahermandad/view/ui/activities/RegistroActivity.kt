f08769a2b4e46eba2e96364b8114020a7d1a2e7f
Jesus Adrian Peñaranda Perez <gsusp18@gmail.com>
2022-11-16 00:15:43 -0500

    cambios registro y mas

--- app/src/main/java/com/example/lahermandad/view/ui/activities/RegistroActivity.kt
+++ app/src/main/java/com/example/lahermandad/view/ui/activities/RegistroActivity.kt
@@ -5,15 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioGroup
 import android.widget.Toast
 import com.example.lahermandad.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
+import java.util.Date
 
 class RegistroActivity : AppCompatActivity() {
     lateinit var registrobutton:Button
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var name:EditText
+    private lateinit var date:EditText
+
+    private lateinit var dbReference:DatabaseReference
+    private lateinit var database:FirebaseDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
@@ -21,8 +31,13 @@ class RegistroActivity : AppCompatActivity() {
         registrobutton = findViewById(R.id.registrobutton)
         val correo=findViewById<EditText>(R.id.correo)
         val contrasena=findViewById<EditText>(R.id.password)
-        val nuevonombre=findViewById<EditText>(R.id.nombreCompleto)
+        name=findViewById(R.id.nombreCompleto)
+        date=findViewById(R.id.FechaN)
+
         val contraseñaconfirmar=findViewById<EditText>(R.id.confcont)
+
+        database=FirebaseDatabase.getInstance()
+        dbReference=database.reference.child("Usuario")
 
 
         registrobutton.setOnClickListener {
@@ -37,9 +52,15 @@ class RegistroActivity : AppCompatActivity() {
         }
     }
     private fun crearUsuario(correo:String, contrasena:String){
+        val newname:String = name.text.toString()
+        val newemail:String = correo
         firebaseAuth.createUserWithEmailAndPassword(correo,contrasena)
             .addOnCompleteListener(this) {
                 Task->if(Task.isSuccessful){
+                    val user=firebaseAuth.currentUser
+                    val userdb=dbReference.child(user?.uid.toString())
+                    userdb.child("Name").setValue(newname)
+                    userdb.child("Email").setValue(newemail)
                     sendEmailVerification()
                     Toast.makeText(baseContext,"Usuario Creado Exitosamente, se requiere verificacion ",Toast.LENGTH_SHORT).show()
                     startActivity(Intent(this,login::class.java))
