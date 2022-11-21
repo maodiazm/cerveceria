ac58238fba31b762658b0940f1b0c4207c73d8fe
Jesus Adrian Peñaranda Perez <gsusp18@gmail.com>
2022-11-20 23:17:07 -0500

    ya porfin finish

--- app/src/main/java/com/example/lahermandad/view/ui/fragments/GpsFragment.kt
+++ app/src/main/java/com/example/lahermandad/view/ui/fragments/GpsFragment.kt
@@ -1,16 +1,24 @@
 package com.example.lahermandad.view.ui.fragments
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.example.lahermandad.R
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.type.LatLng
 
 
 class GpsFragment : Fragment(), OnMapReadyCallback {
@@ -40,9 +48,55 @@ class GpsFragment : Fragment(), OnMapReadyCallback {
 
     }
 
-    override fun onMapReady(p0: GoogleMap) {
+    override fun onMapReady(map: GoogleMap) {
+        val bogota = com.google.android.gms.maps.model.LatLng (4.60971, -74.08175)
+        map?.let {
+            this.googleMap = it
+            map.addMarker(MarkerOptions().position(bogota))
+        }
+        enableLocation()
 
     }
 
+    private fun localizacionPermisos()=ContextCompat.checkSelfPermission(this.requireContext(),
+        Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED
+
+    @SuppressLint("MissingPermission")
+    private fun enableLocation(){
+        if(!::googleMap.isInitialized)return
+        if(localizacionPermisos()){
+            googleMap.isMyLocationEnabled = true
+        }else{
+            requestLocationPermision()
+        }
+    }
+    companion object{
+        const val REQUEST_CODE_LOCATION = 0
+    }
+    private fun requestLocationPermision() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(
+                this.requireActivity(),Manifest.permission.ACCESS_FINE_LOCATION
+        )){
+            Toast.makeText(context,"requiere activar permisos",Toast.LENGTH_SHORT).show()
+        }else{
+            ActivityCompat.requestPermissions(this.requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),REQUEST_CODE_LOCATION)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            REQUEST_CODE_LOCATION ->
+                if(grantResults.isNotEmpty()&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    googleMap.isMyLocationEnabled=true
+                }else{
+                    Toast.makeText(context,"Para activar localizacion, ir a ajustes y activar permisos",Toast.LENGTH_SHORT).show()
+                }else ->{}
+        }
+    }
 
 }
